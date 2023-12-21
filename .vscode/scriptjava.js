f5488da44b84bc7b6a0e431bfc2e10daa71d66ae
Yago Fonseca <yagofonseca18@gmail.com>
2023-12-21 08:41:18 +0000

    add.readme

--- .vscode/scriptjava.js
+++ .vscode/scriptjava.js
@@ -1,16 +1,14 @@
-  function toggleMode(){
-const html = document.documentElement
-html.classList.toggle("light")
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
 
+  const img = document.querySelector("#profile img")
 
- const img = document.querySelector("#profile img")
-
-   if(html.classList.contains("light")){
-    img.setAttribute("src", "./assets/avatar-light.png")
-   }
-
-   else{
+  if(html.classList.contains("light")){
     img.setAttribute("src", "./assets/avatar.png")
-   }
-   
-} 
+  }
+
+  else{
+    img.setAttribute("src", "./assets/avatar-light.png")
+  }
+}
